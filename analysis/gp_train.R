97a0983dd0264109d58ebde8c35dc46cc62de2ea
mpadge <mark.padgham@email.com>
2026-06-11 14:43:35 +0200

    suppress couple more 'file.remove' outputs in analysis scripts

--- analysis/gp_train.R
+++ analysis/gp_train.R
@@ -279,7 +279,9 @@ bsup <- all_bsup [param_names]
 log_dir <- if (!is.null (pars_s$log_dir)) pars_s$log_dir else "/tmp/escalation"
 dir.create (log_dir, recursive = TRUE, showWarnings = FALSE)
 old_done <- list.files (log_dir, pattern = "\\.done$", full.names = TRUE)
-if (length (old_done) > 0) file.remove (old_done)
+if (length (old_done) > 0) {
+    chk <- file.remove (old_done)
+}
 
 fixed <- list (
     n             = as.integer (pars_a$n),
